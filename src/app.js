2e77f17c13e4d6c53f70b88c2659b18dd102703d
stereoIII6 <stereodocbush@gmail.com>
2022-09-16 16:26:41 +0200

    earthliy

--- src/app.js
+++ src/app.js
@@ -68,8 +68,6 @@ const setNewMsg = async () => {
 };
 
 const draw = async () => {
-  const GL = await GreenListData();
-  const MSG = await GL.showMsg();
   console.log(rand, "bg :" + Math.floor(Number(String(rand)[0])), "body :" + Math.floor(Number(String(rand)[1]) / 2), "bubble :" + Math.floor(Number(String(rand)[2]) / 3), "eye :" + String(rand)[3], "mouth :" + String(rand)[4]);
   bg.src = url + "bg/" + Math.floor(Number(String(rand)[0])) + ".png";
   body.src = url + "body/" + Math.floor(Number(String(rand)[1])) + ".png";
@@ -80,8 +78,9 @@ const draw = async () => {
   if (String(rand)[4] == undefined) go = 0;
   else go = String(rand)[4];
   mouth.src = url + "mouth/" + go + ".png";
-  msg.innerHTML = "! BE FRUITY MY FRESH FAM !";
-  msg.innerHTML = MSG;
+  msg.innerHTML = "BE FRUITY MY FRESH FRENZ !";
+  console.log(signer);
+  if (signer._address !== null) msg.innerHTML = MSG;
   const l = msg.innerHTML.length;
   // console.log(l);
   if (l <= 12) msg.style.fontSize = "3em";
